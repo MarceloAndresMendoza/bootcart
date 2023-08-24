297457b76ba086c83c95f241d5728c27973859ca
MarceloAndres <mmendozaf@gmail.com>
2023-08-24 03:02:15 -0400

    feat: Store Cart

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -3,6 +3,7 @@ import { AppIndex } from "../views/AppIndex"
 import { StoreMain } from "../views/StoreMain"
 import { StoreProduct } from "../views/StoreProduct"
 import { AppNotFound } from "../views/AppNotFound"
+import { StoreCart } from "../views/StoreCart"
 
 export const AppRouter = () => {
   return (
@@ -13,6 +14,7 @@ export const AppRouter = () => {
               <Route index element = { <StoreMain /> } />
               <Route path = ":productID" element = { <StoreProduct /> } />
             </Route>
+            <Route path = "cart" element = { <StoreCart /> } />
             <Route path = '*' element = { <AppNotFound /> } />
         </Routes>
     </>
---
+++ src/views/StoreCart.jsx
@@ -0,0 +1,5 @@
+export const StoreCart = () => {
+  return (
+    <div>StoreCart</div>
+  )
+}
