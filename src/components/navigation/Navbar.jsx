a86546f33f77d5e700d3458fa0546088b73b3768
MarceloAndres <mmendozaf@gmail.com>
2023-08-24 02:19:44 -0400

    feat: Navlink added

--- src/components/navigation/Navbar.jsx
+++ src/components/navigation/Navbar.jsx
@@ -1,10 +1,14 @@
+import { NavLink } from "react-router-dom"
+
 export const Navbar = () => {
   return (
     <>
-        <div className="bg-slate-300 h-16 flex items-center px-4">
-            <div className="">
-                Brand
-            </div>
+        <div className="fixed top-0 w-full bg-blue-300 h-16 flex items-center px-4">
+            <ul>
+                <li>
+                    <NavLink to='/' >Brand</NavLink>
+                </li>
+            </ul>
         </div>
     </>
   )
