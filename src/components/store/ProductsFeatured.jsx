9be2922017b7d1829d7d20a53bced0690a74196c
Marcelo Andres <mmendozaf@gmail.com>
2023-08-25 15:30:21 -0400

    chore: cleanup

---
+++ src/components/store/ProductsFeatured.jsx
@@ -0,0 +1,5 @@
+export const ProductsFeatured = () => {
+  return (
+    <div>ProductsFeatured</div>
+  )
+}
---
+++ src/components/store/ProductsFilter.jsx
@@ -0,0 +1,5 @@
+export const ProductsFilter = () => {
+  return (
+    <div>ProductsFilter</div>
+  )
+}
