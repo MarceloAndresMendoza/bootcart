e7bf8889b804578ff80d9cff26fce90080473dae
MarceloAndres <mmendozaf@gmail.com>
2023-08-25 19:16:03 -0400

    style: Now not bottom fixed

--- src/components/navigation/Footer.jsx
+++ src/components/navigation/Footer.jsx
@@ -1,7 +1,7 @@
 export const Footer = () => {
   return (
     <>
-      <div className="fixed bottom-0 flex bg bg-slate-400 w-full h-16 items-center px-4">
+      <div className="flex bg bg-slate-400 w-full h-16 items-center px-4">
         <div>
           Footer
         </div>
