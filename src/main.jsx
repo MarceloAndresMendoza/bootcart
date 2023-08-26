2c3f6074001b226c5ce004666455080d372ab4fd
Marcelo Andres <mmendozaf@gmail.com>
2023-08-26 02:44:19 -0400

    chore: updated some routes

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,8 @@ import { BrowserRouter } from 'react-router-dom';
 import './assets/styles/index.css';
 import './utils/i18next'
 import { AppRouter } from './routes/AppRouter.jsx';
-import { Navbar } from './components/navigation/Navbar.jsx';
-import { Footer } from './components/navigation/Footer';
+import { Navbar } from './components/ui/Navbar.jsx';
+import { Footer } from './components/ui/Footer';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
