f9fea7c64a535507455f013297d4028b08bcd543
MarceloAndres <mmendozaf@gmail.com>
2023-08-27 03:19:33 -0400

    feat: Added small prop to Categories

--- src/views/AppIndex.jsx
+++ src/views/AppIndex.jsx
@@ -2,7 +2,7 @@ import { useTranslation } from 'react-i18next';
 import { StoreTitle } from '../components/store/branding/StoreTitle.jsx'
 import { ProductsFeatured } from '../components/store/products/ProductsFeatured.jsx';
 import { StoreIntroduction } from '../components/store/branding/StoreIntroduction.jsx';
-import { CategoriesSmall } from '../components/store/categories/CategoriesSmall.jsx';
+import { Categories } from '../components/store/categories/Categories.jsx';
 import { StoreInfoIcons } from '../components/store/support/StoreInfoIcons.jsx';
 
 export const AppIndex = () => {
@@ -17,7 +17,7 @@ export const AppIndex = () => {
                 <StoreInfoIcons />
             </div>
             <hr className='mx-4'/>
-            <CategoriesSmall />
+            <Categories small={true}/>
         </>
     )
 }
