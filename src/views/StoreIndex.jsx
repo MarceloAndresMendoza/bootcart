1d8207965c0bf28711aecfd6d668662dc2047dee
MarceloAndres <mmendozaf@gmail.com>
2023-08-27 03:20:04 -0400

    feat: title and category view

--- src/views/StoreIndex.jsx
+++ src/views/StoreIndex.jsx
@@ -1,5 +1,17 @@
+import { StoreTitle } from "../components/store/branding/StoreTitle"
+import { useTranslation } from 'react-i18next';
+import { Categories } from "../components/store/categories/Categories";
+
 export const StoreIndex = () => {
+  const {t, i18n} = useTranslation();
   return (
-    <div>StoreIndex</div>
+    <>
+      <div>
+        <div>
+          <StoreTitle subtitle={t('store-title')}/>
+          <Categories />
+        </div>
+      </div>
+    </>
   )
 }
