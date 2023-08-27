0a27eb7541df4ceafd917a20202576adf1c836ef
MarceloAndres <mmendozaf@gmail.com>
2023-08-27 03:37:45 -0400

    feat: 404 message and cat image

--- src/views/AppNotFound.jsx
+++ src/views/AppNotFound.jsx
@@ -1,5 +1,17 @@
 export const AppNotFound = () => {
+  const handleGoHome = () => {
+    window.location.href = "/";
+  }
   return (
-    <div>AppNotFound</div>
+    <>
+      <div className="p-4">
+        <div className="flex flex-col gap-4 justify-start">
+          <h1 className="text-5xl">Error 404</h1>
+          <h2 className="text-lg">No se ha encontrado el recurso solicitado.</h2>
+          <button className="max-w-3xl bg-blue-500 text-white px-4 py-2 shadow-md hover:bg-blue-600" onClick={handleGoHome}>Volver al inicio</button>
+          <img className="max-w-3xl" src="https://http.cat/404" alt="" />
+        </div>
+      </div>
+    </>
   )
 }
