f9362777b038e6ef0b4eed0df3f5ee3577912de5
MarceloAndres <mmendozaf@gmail.com>
2023-08-29 17:24:09 -0400

    feat: price formatted

--- src/components/ui/smallbits/TableCartRow.jsx
+++ src/components/ui/smallbits/TableCartRow.jsx
@@ -1,14 +1,14 @@
+import { thousandsSeparate } from "../../../utils/stringUtils";
+
 export const TableCartRow = (props) => {
     // TODO
     // Task: make this globally available
     const localCurrency = '$';
     //-- end
     const { product } = props;
-    const { title, shortDescription, price } = product;
 
     // TEST
     const images = [];
-    const quantity = 1;
     //--- END
 
     return (
@@ -17,24 +17,24 @@ export const TableCartRow = (props) => {
                 {images[0]}
             </td>
             <td className='py-1 px-4 text-gray-500 w-4/12 text-sm'>
-                {title}
+                {product.title}
             </td>
             <td className='py-1 px-4 text-gray-500 w-2/12 text-xs'>
-                {shortDescription}
+                {product.shortDescription}
             </td>
             <td className='py-1 px-4 text-blue-500 w-1/12'>
-                {quantity}
+                {product.quantity}
             </td>
             <td className='py-1 px-4 text-blue-700 w-1/12'>
                 <div className="flex flex-row justify-between gap-2">
                     <div>{localCurrency}</div>
-                    <div>{price}</div>
+                    <div>{thousandsSeparate(product.price)}</div>
                 </div>
             </td>
             <td className='py-1 px-4 text-blue-700 w-1/12'>
-                <div className="flex flex-row justify-between gap-2">
+                <div className="flex flex-row justify-between gap-2 font-bold">
                     <div>{localCurrency}</div>
-                    <div>{price}</div>
+                    <div>{thousandsSeparate(product.price * product.quantity)}</div>
                 </div>
             </td>
         </>
