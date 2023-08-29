4e35f1cec5ef62088b7fbaa97dc0ca644c6f3b3c
MarceloAndres <mmendozaf@gmail.com>
2023-08-29 17:21:04 -0400

    style: making room for a price totals value

--- src/components/ui/smallbits/TableCartTotal.jsx
+++ src/components/ui/smallbits/TableCartTotal.jsx
@@ -10,7 +10,7 @@ export const TableCartTotal = (props) => {
             <td colSpan={5} className="w-5/6 font-bold px-4 py-2">
                 {t('table-cart-total-label')}
             </td>
-            <td className="flex flex-row justify-between gap-2 w-1/6 text-blue-900 font-bold py-2">
+            <td className="px-1 flex flex-row justify-between gap-2 w-1/6 text-blue-900 text-lg font-bold py-2">
                 <div>{localCurrency}</div>
                 <div>{price}</div>
             </td>
