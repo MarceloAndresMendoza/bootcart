57567f97a55ae018749864d0e91cc50ef40b8a09
MarceloAndres <mmendozaf@gmail.com>
2023-09-07 04:59:08 -0300

    feat: human date format

--- src/utils/stringUtils.js
+++ src/utils/stringUtils.js
@@ -4,3 +4,12 @@ export const thousandsSeparate = (value) => {
     }
     return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.');
 };
+
+export const simplifyFormatDate = (dateString) => {
+    const date = new Date(dateString);
+    const day = date.getDate().toString().padStart(2, '0');
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const year = date.getFullYear().toString();
+
+    return `${day}-${month}-${year}`;
+}
